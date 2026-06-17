f97d4215bcfb570ad004faaefad013f5fe3cf926
carlos5998 <995517@grundfos.com>
2026-06-17 12:30:27 -0500

    primer paso de paguian web

---
+++ ejercicios.js
@@ -0,0 +1,35 @@
+// 1. Escribe un comentario en una linea
+// Esto es un comentario
+// 2. Escribe un comentario en varias lineas
+// Esto es un comentario
+// en varias lineas
+// en JavaScript
+// 3. Declara variables con valores asociados a todos los datos del tipo primitivo
+    let numerov = 100
+    let textov = "Hola mundo"
+    let booleanov = false
+    let undefinedv 
+    let nullv = null
+    let symbolv = Symbol("simbolo")
+    let bigintv = 742374317327729n
+// 4. Imrprime por consola el valor de todas las variables
+    console.log (numerov)
+    console.log (texto)
+    console.log (booleanov)
+    console.log (undefinedv)
+    console.log (nullv)
+    console.log (symbolv)
+    console.log (bigintv)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuacion, modifica los valores de las variables por otros de mismo tipo
+
+// 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los datos del tipo primitivo
+
+// 9. A continuacion, modifica los valores de las constantes 
+
+// 10. Comenta las lineas que produzcan algun tipo de error al ejecutarse
+
